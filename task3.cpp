95a69eb122fa2e3c230e8612d41b3e772976c0eb
setlol <dovnar.maxim@gmail.com>
2016-06-23 13:54:33 +0300

    homework
    Пользователь вводит любое целое число. Необходимо из этого целого числа удалить все цифры 3 и 6 и вывести обратно на экран.

---
+++ task3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int namberout = 0, namberin;
+
+	cout << "Введите число: ";
+	cin >> namberin;
+
+	while (namberin > 0)
+	{
+		if (namberin % 10 != 3 &&
+			namberin % 10 != 6)
+		{
+			namberout *= 10;
+			namberout += namberin % 10;
+		}
+		namberin /= 10;
+	}
+
+	cout << "Полученное число: ";
+
+	while (namberout > 0)
+	{
+		cout << namberout % 10;
+		namberout /= 10;
+	}
+	cout << endl;
+	return 0;
+}
+
+
+/*Пользователь вводит любое целое число. Необходимо из этого целого числа удалить все цифры 3 и 6 и вывести обратно на экран.*/
