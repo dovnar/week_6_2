58e429150f495b1b7f69942a013d195b5483b59c
setlol <dovnar.maxim@gmail.com>
2016-06-24 13:40:16 +0300

    homework
    Пользователь вводит целое число А. Программа должна определить, что куб суммы цифр этого числа равен А*А

---
+++ task5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int nambercude, namberin, square, namber;
+
+	cout << "Введите число: ";
+	cin >> namberin;
+	namber = namberin;
+	square = namberin  * namberin;
+
+	while (namberin > 0)
+	{
+		nambercude += namberin % 10;
+		namberin /= 10;
+	}
+	if ((nambercude = nambercude * nambercude * nambercude) == square)
+	{
+		cout << "куб суммы цифр " << namber << " числа равен квадрату этого числа";
+	}
+	else
+	{
+		cout << "куб суммы цифр " << namber << " числа неравен квадрату этого числа";
+	}
+	return 0;
+}
+
+
+
+/*Пользователь вводит целое число А. Программа должна определить, что куб суммы цифр этого числа равен А*А */
