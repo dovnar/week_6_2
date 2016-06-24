d37097c365b2051418a766303db4ac119684f485
setlol <dovnar.maxim@gmail.com>
2016-06-24 16:01:10 +0300

    homework
    Пользователь вводит два целых числа. Необходимо вывести все целые числа, на которые оба введенных числа делятся без остатка. 
    Программа для нахождение соответствующего числа

---
+++ taks5.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int nambercude = 0, namberin, square, namber = 1;
+
+	cout << "Введите число: ";
+	while (namber <= 100000000000)
+	{
+		namberin = namber;
+		square = namberin  * namberin;
+
+		while (namberin > 0)
+		{
+			nambercude += namberin % 10;
+			namberin /= 10;
+		}
+		if ((nambercude = nambercude * nambercude * nambercude) == square)
+		{
+			cout << namber << " ";
+		}
+		square = 0;
+		nambercude = 0;
+		namber++;
+	}
+	cout << endl;
+
+	return 0;
+}
