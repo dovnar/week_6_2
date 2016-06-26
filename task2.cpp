cb4fb8f9616a77c371108b3732421231131986f4
introkun <introkun@gmail.com>
2016-06-27 00:00:29 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -7,6 +7,7 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 
+	// опечатка: number
 	int namber = 100, namber1, namber2, namber3, count = 0;
 
 	cout << "Подсчитаем количество целых чисел в диапазоне от 100 до 999 у которых все цифры разные" "\n";
